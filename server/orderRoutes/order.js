9109b813cd4526d246983dd4ce447145a26f5b83
shriyansh2k26 <haythamiles@gmail.com>
2023-11-27 16:55:52 +0530

    new

--- server/orderRoutes/order.js
+++ server/orderRoutes/order.js
@@ -4,32 +4,41 @@ const order=require('../modals/orderModal')
 const orderRoutes=express.Router();
 
 orderRoutes.post('/postorder',async(req,res)=>{
-
+try {
     const {email,orders}=req.body;
-// console.log(req.body)
-    const checkemail= await order.findOne({email:email})
-    if(!checkemail){
-        await order.create({
-            email:email,
-            orders:orders
-        })
-      return  res.json({
-            message:"Order Created",
+    // console.log(req.body)
+        const checkemail= await order.findOne({email:email})
+        if(!checkemail){
+            await order.create({
+                email:email,
+                orders:orders
+            })
+          return  res.json({
+                message:"Order Created",
+                success:true
+            })
+        }
+        await order.updateOne({email:email},{$push:{orders:orders}})
+       return  res.json({
+            message:"Order Updated",
             success:true
         })
-    }
-    await order.updateOne({email:email},{$push:{orders:orders}})
-   return  res.json({
-        message:"Order Updated",
-        success:true
-    })
+} catch (error) {
+ console.log(error)  
+ res.send(error) 
+}
+   
 })
 orderRoutes.get('/getorder',async(req,res)=>{
-
+try {
     const findorder= await order.find();
     res.send({
        order: findorder
     })
+} catch (error) {
+    res.send(error)
+}
+ 
 })
 
 module.exports=orderRoutes;
