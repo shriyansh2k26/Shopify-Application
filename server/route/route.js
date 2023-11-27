757e7509be97e2cdbf65f7c59b14525a939ce0a4
shriyansh2k26 <haythamiles@gmail.com>
2023-11-27 17:01:12 +0530

    new

--- server/route/route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import {registerController} from '../controller/authRegister.js';
-const router=express.Router();
-
-router.post('/register',registerController)
-
-export default router;
